67f6fcc1d17b532d1c90b18ae21127d4c0d4df9e
Noel Ortiz <20175580@itla.edu.do>
2021-10-12 22:38:45 -0400

    contador por metodo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   titulo: string = 'Contador App';
   numero: number = 10; 
+
+  base: number = 5;
+
+ calcular(valor: number){
+   this.numero += valor;
+ }
 }
+
