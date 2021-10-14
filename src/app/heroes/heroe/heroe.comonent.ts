a665f798a31241a62cf0b93c2a5a97b0213b48d1
Noel Ortiz <20175580@itla.edu.do>
2021-10-14 12:20:04 -0400

    One Way data binding

--- src/app/heroes/heroe/heroe.comonent.ts
+++ src/app/heroes/heroe/heroe.comonent.ts
@@ -1,18 +1,28 @@
 import { Component } from "@angular/core";
 
-
 @Component({
     selector: 'app-heroe',
     templateUrl: `heroe.component.html`
 
-}) 
+})
 
+ 
 export class HeroeComponent{
 Nombre: string = 'Thor';
 edad: number = 40;
 
+get nombreCapitalizado(){
+    return this.Nombre.toUpperCase();
+}
 obtenerNombre(): string{
     return `${this.Nombre} - ${this.edad}`; 
+} 
+
+cambiarNombre(){
+    this.Nombre = 'Spiderman';
 }
 
+cambiarEdad(){
+    this.edad = 25;
+}
 }
