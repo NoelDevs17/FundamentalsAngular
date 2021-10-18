632d5ef4e196d93c03f570f3f0c0617e4ceed52e
Noel Ortiz <20175580@itla.edu.do>
2021-10-18 09:41:56 -0400

    ngIf

--- src/app/heroes/listado/listado.component.ts
+++ src/app/heroes/listado/listado.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styleUrls: ['./listado.component.css']
+  styleUrls: ['./listado.component.css'],
+
 })
-export class ListadoComponent implements OnInit {
+export class ListadoComponent  {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+heroes: string[] = ['Spiderman', 'Iroman', 'Hulk', 'Thor', 'capitan america'];
+heroeBorrado: string = '';
+  
+borrarHeroe(){
+  this.heroeBorrado = this.heroes.shift() || '';
+  
+}
 }
